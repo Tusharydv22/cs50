1aa1652b9a6eecf5dda9b209661b88ff4c47aa29
Tusharydv22 <tusharydv22@gmail.com>
2022-11-16 16:35:41 +0530

    git commit

--- meow.c
+++ meow.c
@@ -5,12 +5,18 @@ int main (void)
 {
     int counter = 0;
    
-    
+    //while loop  
     while( counter < 5)
     {
         printf(" Meow! \n");
         counter++;   
 
+    }
+
+    // For loop
+    for(int i = 0; i < 3; i++ )
+    {
+        printf(" Groot!!\n")
     }
     return 0;
 }
